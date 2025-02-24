c9016401e8f783ab7411a630de2130a2987a2cb2
Rida <ridachaarani@yahoo.com>
2025-02-23 21:43:03 -0500

    Fix esling config for TypeScript

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
-import eslint from "eslint";
 import prettier from "eslint-plugin-prettier";
 import prettierConfig from "eslint-config-prettier";
+import typescriptEslintPlugin from "@typescript-eslint/eslint-plugin";
+import typescriptEslintParser from "@typescript-eslint/parser";
 
 export default [
   {
@@ -8,14 +9,20 @@ export default [
     languageOptions: {
       ecmaVersion: 2021,
       sourceType: "module",
+      parser: typescriptEslintParser,
+
     },
     plugins: {
       prettier,
+      "@typescript-eslint": typescriptEslintPlugin,
     },
     rules: {
       ...prettierConfig.rules,
       "no-console": "warn",
       "no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": ["warn"],
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
     },
   },
 ];
